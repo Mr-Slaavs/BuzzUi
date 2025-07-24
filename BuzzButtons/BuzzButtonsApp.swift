bdcf8f0246a48d0bb7ec1a54ceb563d6d7d27ec4
Mr-Slaavs <alexslavny@gmail.com>
2025-07-24 16:35:51 -0700

    Initial commit

---
+++ BuzzButtons/BuzzButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzButtonsApp.swift
+//  BuzzButtons
+//
+//  Created by Alexander Slavny on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BuzzButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuzzButtonsTests/BuzzButtonsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzButtonsTests.swift
+//  BuzzButtonsTests
+//
+//  Created by Alexander Slavny on 7/24/25.
+//
+
+import Testing
+@testable import BuzzButtons
+
+struct BuzzButtonsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
